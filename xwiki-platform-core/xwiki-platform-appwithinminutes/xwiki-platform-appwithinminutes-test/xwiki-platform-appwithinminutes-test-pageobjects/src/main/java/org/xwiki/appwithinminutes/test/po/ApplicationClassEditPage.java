ca65a61d2482e3f0d001b7605a047d50bab82fe4
Manuel Leduc <manuel.leduc@xwiki.com>
2021-03-26 11:47:23 +0100

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-appwithinminutes/xwiki-platform-appwithinminutes-test/xwiki-platform-appwithinminutes-test-pageobjects/src/main/java/org/xwiki/appwithinminutes/test/po/ApplicationClassEditPage.java
+++ xwiki-platform-core/xwiki-platform-appwithinminutes/xwiki-platform-appwithinminutes-test/xwiki-platform-appwithinminutes-test-pageobjects/src/main/java/org/xwiki/appwithinminutes/test/po/ApplicationClassEditPage.java
@@ -148,14 +148,14 @@ public class ApplicationClassEditPage extends ApplicationEditPage
      */
     public static ApplicationClassEditPage goToEditor(DocumentReference reference)
     {
-        getUtil().gotoPage(reference, "edit",
-            "editor", "inline", "template", "AppWithinMinutes.ClassTemplate", "title",
-            reference.getName() + " Class");
+        getUtil().gotoPage(reference, "edit", "editor", "inline", "template", "AppWithinMinutes.ClassTemplate", "title",
+            reference.getLastSpaceReference().getName() + " Class");
         return new ApplicationClassEditPage();
     }
 
     /**
      * Creates a new application with the given name.
+     *
      * @param appName the application name
      * @return the corresponding page object
      * @since 13.2

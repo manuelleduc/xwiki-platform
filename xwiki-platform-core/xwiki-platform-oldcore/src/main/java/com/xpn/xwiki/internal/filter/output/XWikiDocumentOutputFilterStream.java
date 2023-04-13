8d0e8bef23ad1185ccc06a5d07cf7a519f4e06d2
Manuel Leduc <manuel.leduc@xwiki.com>
2023-04-13 09:22:24 +0200

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/filter/output/XWikiDocumentOutputFilterStream.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/filter/output/XWikiDocumentOutputFilterStream.java
@@ -352,8 +352,6 @@ public class XWikiDocumentOutputFilterStream extends AbstractEntityOutputFilterS
         getBaseClassOutputFilterStream().setEntity(this.entity.getXClass());
     }
 
-    
-
     private void end(FilterEventParameters parameters)
     {
         // Set content

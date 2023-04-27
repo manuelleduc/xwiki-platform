a5e98e0e0947e86d0092a27045f7d1b497dbeb97
Manuel Leduc <manuel.leduc@xwiki.com>
2023-04-27 15:10:31 +0200

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
@@ -4641,6 +4641,7 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable
             doc.originalDocument = this.originalDocument;
 
             doc.requiredRights = new DefaultRequiredRights(doc, this.requiredRights.getRights());
+            doc.requiredRightsActivated = this.requiredRightsActivated;
         } catch (Exception e) {
             // This should not happen
             LOGGER.error("Exception while cloning document", e);
@@ -9585,7 +9586,7 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable
      * @since 15.4RC1
      */
     @Unstable
-    public Boolean getRequiredRightsActivated()
+    public Boolean isRequiredRightsActivated()
     {
         return this.requiredRightsActivated;
     }

25649b09914e1121b15d524e0725a9ed97293224
Manuel Leduc <manuel.leduc@xwiki.com>
2023-06-26 15:33:51 +0200

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-security/xwiki-platform-extension-security-api/src/main/java/org/xwiki/extension/security/internal/SolrToLiveDataEntryMapper.java
+++ xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-security/xwiki-platform-extension-security-api/src/main/java/org/xwiki/extension/security/internal/SolrToLiveDataEntryMapper.java
@@ -20,8 +20,6 @@
 package org.xwiki.extension.security.internal;
 
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.IntFunction;

925998be26ec2f8809f86ea5bcdf2a296dc4a149
Manuel Leduc <manuel.leduc@xwiki.com>
2024-03-28 18:20:22 +0100

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-test/xwiki-platform-ckeditor-test-docker/src/test/it/org/xwiki/ckeditor/test/ui/ImageIT.java
+++ xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-test/xwiki-platform-ckeditor-test-docker/src/test/it/org/xwiki/ckeditor/test/ui/ImageIT.java
@@ -859,6 +859,8 @@ class ImageIT extends AbstractCKEditorIT
         // Verify that the content matches what we did using CKEditor.
         assertEquals("1. Item 1\n"
             + "1. Item 2 [[image:image.gif]]", savedPage.editWiki().getContent());
+
+        throw new RuntimeException("SUCCESS SNAP");
     }
 
     /**
